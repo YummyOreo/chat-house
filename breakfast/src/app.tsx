c7b20a469e4a725f10874fd3622c244081565fb7
OreoDivision <Bobgim20@gmail.com>
2021-04-17 09:10:18 -0500

    🔌Adds what i can to the react files

--- breakfast/src/app.tsx
+++ breakfast/src/app.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
 import { BrowserRouter, Route } from 'react-router-dom';
-
+// Gets everything
 import Home from "./Components/home/Home";
 import Chat from "./Components/chat/Chat";
 
 const App = () => (
 	<BrowserRouter>
-		<Route path="/" exact component={Home} />
+		<Route path="/" exact component={Home} /> 
 		<Route path="/chat" component={Chat} />
 	</BrowserRouter>
 )
--- breakfast/src/index.tsx
+++ breakfast/src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+// Imports everything needed
 import App from './app';
-
+// Renders
 ReactDOM.render(<App />, document.getElementById('root'));
